722bee98abaa5eb9705779dd97a592c501c975ed
FSPABLO <josefdezs2110@gmail.com>
2026-05-19 12:52:06 -0600

    feat: setup inicial backend bolsaempleo

---
+++ src/test/java/una/ac/cr/bolsaempleo/BolsaempleoApplicationTests.java
@@ -0,0 +1,13 @@
+package una.ac.cr.bolsaempleo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolsaempleoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
